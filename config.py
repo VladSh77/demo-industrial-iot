cfd745eeb753ba4801c521fab60939d8ebffe2a1
Volodymyr <154033554+VladSh77@users.noreply.github.com>
2026-01-20 21:57:07 +0100

    Add configuration settings for Odoo connection

---
+++ config.py
@@ -0,0 +1,5 @@
+# Fayna Digital: Enterprise Security Layer
+ODOO_URL = "https://fayna-demo.odoo.com"
+ODOO_DB = "production_01"
+ODOO_USER = "admin@fayna.agency"
+ODOO_PASSWORD = "API_KEY_PROTECTED"
